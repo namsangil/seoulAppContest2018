bfbfe22b6d20066872dc3bb34a4ae9a1922b26f9
nam <iolk1001@naver.com>
2018-08-07 21:26:07 +0900

    슬라이드 뷰 위치 재구성

--- app/src/main/java/appcontest/seoulsi_we/MainActivity.kt
+++ app/src/main/java/appcontest/seoulsi_we/MainActivity.kt
@@ -1,19 +1,21 @@
 package appcontest.seoulsi_we
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.Toast
 
 /**
  * Created by nam on 2018. 8. 7..
  */
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private var mDrawer: DrawerLayout? = null
     private var toolbar: Toolbar? = null
     private var nvDrawer: NavigationView? = null
@@ -34,48 +36,38 @@ class MainActivity : AppCompatActivity(){
 
         // 슬라이드 뷰 등록
         nvDrawer = findViewById(R.id.nvView)
-        setupDrawerContent(nvDrawer)
+
+        // 뷰 로딩이 완료되면, status bar 높이만큼 떨어뜨린다.
+        nvDrawer?.viewTreeObserver?.addOnGlobalLayoutListener(
+                object : ViewTreeObserver.OnGlobalLayoutListener {
+                    override fun onGlobalLayout() {
+                        val rect = Rect()
+                        val window = window
+                        window.decorView.getWindowVisibleDisplayFrame(rect)
+                        val statusBarHeight = rect.top
+                        nvDrawer?.setPadding(0, statusBarHeight, 0, 0)
+                        nvDrawer?.viewTreeObserver?.removeOnGlobalLayoutListener(this)
+                    }
+                }
+        )
+
     }
 
-    //region 슬라이드 메뉴 관련 메소드
+//region 슬라이드 메뉴 관련 메소드
 
-    private fun setupDrawerContent(navigationView: NavigationView?) {
-        navigationView!!.setNavigationItemSelectedListener { menuItem ->
-            selectDrawerItem(menuItem)
-            true
-        }
-    }
-
-    private fun selectDrawerItem(menuItem: MenuItem) {
-        // Create a new fragment and specify the fragment to show based on nav item clicked
-        //        Fragment fragment = null;
-        //        Class fragmentClass;
-
-        // 각 슬라이드 메뉴의 버튼을 클릭하면 이벤트 발생
-        when (menuItem.itemId) {
+    fun onClickSlideMenu(v: View) {
+        when (v.id) {
             R.id.about_seoulsiwi          // 서울시위란? 버튼
             -> Toast.makeText(this@MainActivity, getString(R.string.about_seoulsiwi), Toast.LENGTH_SHORT).show()
             R.id.about_demo               // 집회시위 관련정보 버튼
             -> Toast.makeText(this@MainActivity, getString(R.string.about_demo), Toast.LENGTH_SHORT).show()
             R.id.promotion_demo           // 집회시위 홍보하기 버튼
             -> Toast.makeText(this@MainActivity, getString(R.string.promotion_demo), Toast.LENGTH_SHORT).show()
-        }
+            R.id.setting                  // 설정 버튼
+            -> Toast.makeText(this@MainActivity, getString(R.string.setting), Toast.LENGTH_SHORT).show()
 
-        //        try {
-        //            fragment = (Fragment) fragmentClass.newInstance();
-        //        } catch (Exception e) {
-        //            e.printStackTrace();
-        //        }
-        //
-        //        // Insert the fragment by replacing any existing fragment
-        //        FragmentManager fragmentManager = getSupportFragmentManager();
-        //        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-        //
-        //        // Highlight the selected item has been done by NavigationView
-        //        menuItem.setChecked(true);
-        //        // Set action bar title
-        //        setTitle(menuItem.getTitle());
-        // Close the navigation drawer
+
+        }
         mDrawer!!.closeDrawers()
     }
 
