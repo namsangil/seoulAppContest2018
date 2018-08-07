6470716b6f1ad59b7e22b0706e1bf8580f97bd5d
nam <iolk1001@naver.com>
2018-08-07 19:59:52 +0900

    코틀린으로 변경

---
+++ app/src/main/java/appcontest/seoulsi_we/IntroActivity.kt
@@ -0,0 +1,33 @@
+package appcontest.seoulsi_we
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+class IntroActivity : Activity() {
+
+    private val handler = Handler()
+
+    private val runnable = Runnable {
+        val intent = Intent(this@IntroActivity, MainActivity::class.java)
+        this@IntroActivity.startActivity(intent)
+        this@IntroActivity.finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable, 2500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
