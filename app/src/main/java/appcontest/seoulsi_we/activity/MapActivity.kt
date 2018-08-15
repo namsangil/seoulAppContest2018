b264a179c1cab330d491a9fffc31e805262e9007
nam <iolk1001@naver.com>
2018-08-15 22:26:07 +0900

    맵 1차

--- app/src/main/java/appcontest/seoulsi_we/activity/MapActivity.kt
+++ app/src/main/java/appcontest/seoulsi_we/activity/MapActivity.kt
@@ -2,7 +2,9 @@ package appcontest.seoulsi_we.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.RelativeLayout
+import android.widget.TextView
 import appcontest.seoulsi_we.R
 import net.daum.mf.map.api.MapView
 
@@ -12,8 +14,17 @@ class MapActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        val textView : TextView = findViewById(R.id.navigation_toolbar_title)
+        textView.text = getString(R.string.realtime_map)
+
         val mapview = MapView(this@MapActivity)
         val layout = findViewById<RelativeLayout>(R.id.map_view)
         layout.addView(mapview)
     }
+
+    fun backImageClick(v : View){
+        finish()
+    }
 }
+
+
